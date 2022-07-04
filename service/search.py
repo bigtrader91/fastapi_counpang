d88cdc69fb959f62fa2e237086d3bff24c079590
bigtrader91 <dahy949@gmail.com>
2022-07-04 14:14:08 +0000

    add crontab

--- service/search.py
+++ service/search.py
@@ -1,4 +1,5 @@
 import time
+import os
 import pandas as pd
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
@@ -7,14 +8,18 @@ import urllib.request
 import pandas as pd
 import joblib
 from service import 연관검색어, 쿠팡검색기, insert_data
+import datetime
+print(os.path.realpath(__file__))
+nowT = datetime.datetime.now()
+
+
 telgm_token = '1108135935:AAEzD9fUZxII258ELQm3ah_gej1E3LqLlmU'
 chat_id=1069639277
 bot = telegram.Bot(token = telgm_token)
-if_exists='replace'
-
+if_exists='append'
 
 engine = create_engine("postgresql://search:1234@localhost/coupang",echo=True)
-keyword_list=joblib.load('키워드모음2.pkl')
+keyword_list=joblib.load('/home/gumeisbuy/web/service/키워드모음2.pkl')
 
 key=keyword_list.pop()
 search_url = f"/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword={urllib.parse.quote(key)}&limit=10&subId=wordpress"
@@ -25,12 +30,13 @@ df['productPrice']=df['productPrice'].apply(lambda x: format(x, ',d'))
 df['tag']=df['keyword']
 df['tag']=df['tag'].apply(lambda x: 연관검색어(x)) #연관검색어 열 추가
 insert_data('search',df, engine, if_exists=if_exists)
-bot.send_message(chat_id=chat_id, text=f'{key} 키워드 추가')
-joblib.dump(keyword_list,'키워드모음2.pkl')
+bot.send_message(chat_id=chat_id, text=f'시간 : {nowT}  |  키워드 : {key}')
+joblib.dump(keyword_list,'/home/gumeisbuy/web/service/키워드모음2.pkl')
+
 
 # while True:
 #     if len(keyword_list)==0:
-#                 
+                
 #         break
 #     key=keyword_list.pop()
 #     쿠팡검색기
--- test.py
+++
@@ -1,7 +0,0 @@
-import joblib
-x=[1,2,3,4,5]
-
-
-
-joblib.dump(x,'test.pkl')
-# df_test=joblib.load('df_2020.pkl')
